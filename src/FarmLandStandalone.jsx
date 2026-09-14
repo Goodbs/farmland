b1debb082c0623a6604fb9f9ecab8344b1b635c3
Goodbs <152873909+Goodbs@users.noreply.github.com>
2026-09-14 15:22:26 +0530

    Create FarmLandStandalone.jsx

---
+++ src/FarmLandStandalone.jsx
@@ -0,0 +1,12 @@
+import AppRouter from "./AppRouter";
+import "./app.css";
+
+function FarmLandStandalone() {
+  return (
+    <div className="farmland-standalone">
+      <AppRouter />
+    </div>
+  );
+}
+
+export default FarmLandStandalone;
