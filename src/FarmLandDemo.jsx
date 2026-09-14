ee89dab64fa9c2bd8b74cd39fefec8d447635bac
Goodbs <152873909+Goodbs@users.noreply.github.com>
2026-09-14 15:23:18 +0530

    Create FarmLandDemo.jsx

---
+++ src/FarmLandDemo.jsx
@@ -0,0 +1,12 @@
+import { Sprout, ArrowRight, ShieldCheck, Wallet } from "lucide-react";
+
+import AppRouter from "./AppRouter";
+import "./app.css";
+
+function FarmLandDemo() {
+  return (
+    <AppRouter />
+  );
+}
+
+export default FarmLandDemo;
