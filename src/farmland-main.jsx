43c6db66ff1c1d6ff139ec25b427a341a3d6e512
Goodbs <152873909+Goodbs@users.noreply.github.com>
2026-09-14 15:21:34 +0530

    Create farmland-main.jsx

---
+++ src/farmland-main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import FarmLandApp from "./FarmLandApp";
+import "./index.css";
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+  <React.StrictMode>
+    <FarmLandApp />
+  </React.StrictMode>
+);
